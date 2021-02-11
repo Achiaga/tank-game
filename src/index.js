5cadd9a8100de53ed0d939a6ac326b2b50fa7f16
Achiaga <alfonso.achiaga@gmail.com>
2021-02-11 15:11:26 +0100

    fixed export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import Game from './ammo-tank'
-// import Game from "./car";
-// import Game from "./car-clean";
-// import Game from "./tank";
+import Game from './ammo-tank';
 
-export default Game
+export default Game;
